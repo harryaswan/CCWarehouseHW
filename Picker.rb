d769533649c5d2860b0d631764edeeb85812ea9d
Harry A. Swan <has.land@hotmail.co.uk>
2016-04-30 15:08:20 +0100

    Conpleted and working

--- Picker.rb
+++ Picker.rb
@@ -3,54 +3,76 @@ require('pry-byebug')
 
 INVENTORY = {
     a: ["needle", "stop sign", "blouse", "hanger", "rubber duck", "shovel", "bookmark", "model car", "glow stick", "rubber band"],
-    b: ["tyre swing", "sharpie", "picture frame", "photo album", "nail filer", "tooth paste", "bath fizzers", "deodorant", "tissue box", "cookie jar"],
+    b: ["tyre swing", "sharpie", "picture frame", "photo album", "nail filer", "tooth paste", "bath fizzers", "tissue box", "deodorant", "cookie jar"],
     c: ["rusty nail", "drill press", "chalk", "word search", "thermometer", "face wash", "paint brush", "candy wrapper", "shoe lace", "leg warmers"]
 }
 
 
-def item_at_bay(bay)
+def item_at_bay(bay) # For task 1
     bay_split = bay.split(//, 2)
     return INVENTORY[bay_split[0].to_sym][bay_split[1].to_i - 1]
 end
 
-def bay_for_item(item)
-
+def bay_for_item(item) # For task 2
     for x in INVENTORY.keys
         if item_index = find_item_index(x, item)
             return "#{x}#{item_index+1}"
         end
     end
-
 end
 
-def find_item_index(sym, item_string)
-    if index = INVENTORY[sym].index(item_string)
-        return index
-    end
-    return false
-end
-
-def items_in_multibay(bays)
+def items_in_multibay(bays) # For task 3
     items_arr = []
-    for x in bays.split(", ")
-        items_arr << item_at_bay(x)
+    if bays.is_a? String
+        for x in bays.split(", ")
+            items_arr << item_at_bay(x)
+        end
+    elsif bays.is_a? Array
+        for x in bays
+            items_arr << item_at_bay(x)
+        end
     end
-
     return items_arr
 end
 
-def bays_for_multiitems(items, arrange=true)
+def bays_for_multiitems(items) # For task 4
     bays_arr = []
-    for x in items.split(", ")
-        bays_arr << bay_for_item(x)
+    if items.is_a? String
+        for x in items.split(", ")
+            bays_arr << bay_for_item(x)
+        end
+    elsif items.is_a? Array
+        for x in items
+            bays_arr << bay_for_item(x)
+        end
     end
+    return bays_arr
 
-    return group_by_bay(bays_arr, arrange)
+    # could be grouped by bay by calling:
+    # return group_by_bay(bays_arr, false)
 end
 
-def group_by_bay(arr, arrange=true)
+def calculate_distance_between_bays(bays_as_string) # For task 5
+    items = items_in_multibay(bays_as_string)
+    bays = bays_for_multiitems(items)
+    return [items, calc_dist_bays(bays)]
+end
 
-    #puts arr
+def order_bays_by_picking(items_as_string)
+    bays = bays_for_multiitems(items_as_string)
+    return group_by_bay(bays)
+    return
+end
+
+# *************************
+# Below is the extra function I created to aid in the functionality of the fucions above
+# *************************
+
+def group_by_bay(arr, arrange=true)
+# the arrange value determines if the program is to output
+# the values in the order that they are to be found or just grouped
+# by the rack letter. The default action is to arrange them in the
+# order you would find them (a10 - a1, c1 - c10, b1 - b10)
 
     alpha = ("a".."z").to_a
 
@@ -62,16 +84,13 @@ def group_by_bay(arr, arrange=true)
 
 
     grouped_arr = []
-    text = []
 
     x = 0
     until x >= bays.length
-        text[x] = []
         if bays[x] != nil && bays[x] != []
             bays[x] = sort_arr(bays[x])
             for a in bays[x]
                 grouped_arr << a
-                text[x] << a
             end
 
         end
@@ -79,26 +98,34 @@ def group_by_bay(arr, arrange=true)
     end
 
     if arrange
-        text[0] = []
+        temp_for_a = []
         for a in bays[0].reverse()
-            text[0] << a
+            temp_for_a << a
         end
-        #arr_text = "#{text[0]}#{text[2]}#{text[1]}"[0...-2]
-
-        return [text[0],text[2],text[1]].flatten()
+        return [temp_for_a,bays[2],bays[1]].flatten()
     else
         return grouped_arr
     end
 
 end
 
+
+def find_item_index(sym, item_string)
+    if index = INVENTORY[sym].index(item_string)
+        return index
+    end
+    return false
+end
+
+
 def sort_arr(arr)
 
     swapped = true
     while swapped do
         swapped = false
-        i = 0
-        loop do
+        # i = 0
+        # loop do
+        for i in 0..(arr.length - 2)
             if arr[i].split(//, 2)[1].to_i > arr[i+1].split(//, 2)[1].to_i
                 # if arr[i].split(//, 2)[0] == arr[i+1].split(//, 2)[0]
                 tmp = arr[i]
@@ -107,8 +134,7 @@ def sort_arr(arr)
                 swapped = true
                 # end
             end
-            break if i == arr.length - 2
-            i += 1
+            # break if i == arr.length - 2
         end
     end
 
@@ -118,18 +144,36 @@ end
 
 
 
-    #
-    # bays_list = []
-    #
-    # for l in ["a".."z"]
-    #     for n in [0..9]
-    #         bays_list << "#{l}#{n}"
-    #     end
-    # end
-    #
-    #
 
 
+def calc_dist_bays(bays_list)
+    bays_name_list = []
 
-# group_by_bay(["a1", "b1", "c1"])
-#puts group_by_bay(["a1", "b6", "c3", "b10", "b1", "c1", "c2", "c10"])
+    for n in 1..10
+        bays_name_list << "a#{n}"
+    end
+
+    bays_name_list = bays_name_list.reverse()
+
+    for l in ["c", "b"]
+        for n in 1..10
+            bays_name_list << "#{l}#{n}"
+        end
+    end
+
+    for l in "d".."z"
+        for n in 1..10
+            bays_name_list << "#{l}#{n}"
+        end
+    end
+
+
+    bay_index_list = []
+
+    for x in bays_list
+        bay_index_list << bays_name_list.index(x)
+    end
+
+    return (bay_index_list.max - bay_index_list.min)
+
+end
--- specs/picker_spec.rb
+++ specs/picker_spec.rb
@@ -3,34 +3,44 @@ require('pry-byebug')
 require_relative('../Picker.rb')
 class PickerTest < Minitest::Test
 
-    # def test_item_at_bay()
-    #     test_result = item_at_bay("b5")
-    #     actual_result = "nail filer"
-    #     assert_equal(actual_result,test_result)
-    # end
-    # def test_bay_for_item()
-    #     test_result = bay_for_item("nail filer")
-    #     actual_result = "b5"
-    #     assert_equal(actual_result,test_result)
-    # end
-    # def test_items_in_multibay()
-    #     test_result = items_in_multibay("b5, b10, b6")
-    #     actual_result = ["nail filer", "cookie jar", "tooth paste"]
-    #     assert_equal(actual_result,test_result)
-    # end
-    def test_bays_for_multiitems()
-        test_result = bays_for_multiitems("shoe lace, sharpie, hanger, rusty nail, leg warmers", false)
+    def test_item_at_bay() # task 1
+        test_result = item_at_bay("b5")
+        actual_result = "nail filer"
+        assert_equal(actual_result,test_result)
+    end
+    def test_bay_for_item() # task 2
+        test_result = bay_for_item("nail filer")
+        actual_result = "b5"
+        assert_equal(actual_result,test_result)
+    end
+    def test_items_in_multibay() # task 3
+        test_result = items_in_multibay("b5, b10, b6")
+        actual_result = ["nail filer", "cookie jar", "tooth paste"]
+        assert_equal(actual_result,test_result)
+    end
+    def test_bays_for_multiitems() # task 4
+        test_result = bays_for_multiitems("shoe lace, sharpie, hanger, rusty nail, leg warmers")
+        actual_result = ["c9", "b2", "a4", "c1", "c10"]
+        assert_equal(actual_result,test_result)
+    end
+    def test_calculate_distance_between_bays() # task 5
+        test_result = calculate_distance_between_bays("b5, b10, b6")
+        actual_result = [["nail filer", "cookie jar", "tooth paste"], 5]
+        assert_equal(actual_result,test_result)
+    end
+    def test_calculate_distance_between_bays2() # task 5
+        test_result = calculate_distance_between_bays("b3, c7, c9, a3")
+        actual_result = [["picture frame", "paint brush", "shoe lace", "blouse"], 15]
+        assert_equal(actual_result,test_result)
+    end
+    def test_order_bays_by_picking() # task 6
+        test_result = order_bays_by_picking("shoe lace, rusty nail, leg warmers")
         actual_result = ["c1", "c9", "c10"]
         assert_equal(actual_result,test_result)
     end
-    def test_group_bays()
-        test_result = group_by_bay(["b6", "c3", "b10", "b1", "c1", "c2", "c10"], false)
-        actual_result = ["b1", "b6", "b10", "c1", "c2", "c3", "c10"]
-        assert_equal(actual_result,test_result)
-    end
-    def test_group_bays_arrange()
-        test_result = group_by_bay(["b6", "c3", "a10", "b1", "c1", "a3"], true)
-        actual_result = ["a10", "a3", "c1", "c3", "b1", "b6"]
+    def test_order_bays_by_picking2() # task 6
+        test_result = order_bays_by_picking("hanger, deodorant, candy wrapper, rubber band")
+        actual_result = ["a10", "a4", "c8", "b9"]
         assert_equal(actual_result,test_result)
     end
 
