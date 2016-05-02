29e3fae5a300ae27a189b146793edc2fc602ac97
Harry A. Swan <has.land@hotmail.co.uk>
2016-05-02 08:03:45 +0100

    Map function added

--- Picker.rb
+++ Picker.rb
@@ -7,7 +7,6 @@ INVENTORY = {
     c: ["rusty nail", "drill press", "chalk", "word search", "thermometer", "face wash", "paint brush", "candy wrapper", "shoe lace", "leg warmers"]
 }
 
-
 def item_at_bay(bay) # For task 1
     bay_split = bay.split(//, 2)
     return INVENTORY[bay_split[0].to_sym][bay_split[1].to_i - 1]
@@ -19,6 +18,7 @@ def bay_for_item(item) # For task 2
             return "#{x}#{item_index+1}"
         end
     end
+    return nil
 end
 
 def items_in_multibay(bays) # For task 3
@@ -32,7 +32,7 @@ def items_in_multibay(bays) # For task 3
             items_arr << item_at_bay(x)
         end
     end
-    return items_arr
+    return items_arr != [] ? items_arr : nil
 end
 
 def bays_for_multiitems(items) # For task 4
@@ -46,8 +46,7 @@ def bays_for_multiitems(items) # For task 4
             bays_arr << bay_for_item(x)
         end
     end
-    return bays_arr
-
+    return bays_arr != [] ? bays_arr : nil
     # could be grouped by bay by calling:
     # return group_by_bay(bays_arr, false)
 end
@@ -58,10 +57,14 @@ def calculate_distance_between_bays(bays_as_string) # For task 5
     return [items, calc_dist_bays(bays)]
 end
 
-def order_bays_by_picking(items_as_string)
+def order_bays_by_picking_from_items(items_as_string)
     bays = bays_for_multiitems(items_as_string)
-    return group_by_bay(bays)
-    return
+    return group_by_bay(bays, true)
+end
+
+def order_bays_by_picking_from_bays(bays_as_string)
+    bays = bays_for_multiitems(items_in_multibay(bays_as_string))
+    return group_by_bay(bays, true)
 end
 
 # *************************
@@ -73,42 +76,36 @@ def group_by_bay(arr, arrange=true)
 # the values in the order that they are to be found or just grouped
 # by the rack letter. The default action is to arrange them in the
 # order you would find them (a10 - a1, c1 - c10, b1 - b10)
-
     alpha = ("a".."z").to_a
-
-    bays = Array.new(3) { Array.new() }
-
+    bays = Array.new(26) { Array.new() }
     for x in arr
         bays[alpha.index(x.split(//, 2)[0])] << x
     end
-
-
     grouped_arr = []
-
-    x = 0
-    until x >= bays.length
+    for x in 0..bays.length
         if bays[x] != nil && bays[x] != []
             bays[x] = sort_arr(bays[x])
             for a in bays[x]
                 grouped_arr << a
             end
-
         end
-        x += 1
     end
-
     if arrange
         temp_for_a = []
         for a in bays[0].reverse()
             temp_for_a << a
         end
-        return [temp_for_a,bays[2],bays[1]].flatten()
+        answer_arr = [temp_for_a,bays[2],bays[1]]
+        for b_i in 3..bays.length
+            if bays[b_i] != nil && bays[b_i] != []
+                answer_arr << bays[b_i]
+            end
+        end
+        return answer_arr.flatten()
     else
         return grouped_arr
     end
-
 end
-
 
 def find_item_index(sym, item_string)
     if index = INVENTORY[sym].index(item_string)
@@ -117,63 +114,83 @@ def find_item_index(sym, item_string)
     return false
 end
 
-
 def sort_arr(arr)
-
     swapped = true
     while swapped do
         swapped = false
-        # i = 0
-        # loop do
         for i in 0..(arr.length - 2)
             if arr[i].split(//, 2)[1].to_i > arr[i+1].split(//, 2)[1].to_i
-                # if arr[i].split(//, 2)[0] == arr[i+1].split(//, 2)[0]
                 tmp = arr[i]
                 arr[i] = arr[i+1]
                 arr[i+1] = tmp
                 swapped = true
-                # end
             end
-            # break if i == arr.length - 2
         end
     end
-
     return arr
-
 end
 
-
-
-
-
 def calc_dist_bays(bays_list)
-    bays_name_list = []
+    bays_name_list = get_bay_list()
+    bay_index_list = []
+    for x in bays_list
+        bay_index_list << bays_name_list.index(x)
+    end
+    return (bay_index_list.max - bay_index_list.min)
+end
 
+def get_bay_list()
+    bays_name_list = []
     for n in 1..10
         bays_name_list << "a#{n}"
     end
-
     bays_name_list = bays_name_list.reverse()
-
     for l in ["c", "b"]
         for n in 1..10
             bays_name_list << "#{l}#{n}"
         end
     end
-
     for l in "d".."z"
         for n in 1..10
             bays_name_list << "#{l}#{n}"
         end
     end
+    return bays_name_list
+end
 
+def print_collection_map(item_or_bays)
 
-    bay_index_list = []
+    if bays = order_bays_by_picking_from_bays(item_or_bays)
+        puts "test".magenta
+    end
+    bays_name_list = get_bay_list()
 
-    for x in bays_list
-        bay_index_list << bays_name_list.index(x)
+    print "\n\n\n     "
+
+    c_line_1 = ""
+    c_line_2 = ""
+    for c in (10..19)
+        c_line_1 << "#{bays_name_list[c]} "
+        if bays.include?(bays_name_list[c])
+            c_line_2 << "/\\ "
+        else
+            c_line_2 << "   "
+        end
     end
 
-    return (bay_index_list.max - bay_index_list.min)
+    print "#{c_line_1}\n     #{c_line_2.green}\n"
+    print "     "
+    print "-"*30
+    print "\n"
+    for ab in (0..9)
+        print bays_name_list[ab].length == 2 ? "#{bays_name_list[ab]} " : "#{bays_name_list[ab]}"
+        print bays.include?(bays_name_list[ab]) ? "<-".green : "  "
+        print "|"
+        print " "*28
+        print "|"
+        print bays.include?(bays_name_list[ab+20]) ? "->".green : "  "
+        print bays_name_list[ab+20] == 2 ? " #{bays_name_list[ab+20]}" : "#{bays_name_list[ab+20]}"
+        print "\n"
+    end
 
 end
--- specs/picker_spec.rb
+++ specs/picker_spec.rb
@@ -1,47 +1,76 @@
 require('minitest/autorun')
 require('minitest/rg')
 require('pry-byebug')
+require('colorize')
 require_relative('../Picker.rb')
+
 class PickerTest < Minitest::Test
+
+    # *************** TASK 1 ***************
 
     def test_item_at_bay() # task 1
         test_result = item_at_bay("b5")
         actual_result = "nail filer"
         assert_equal(actual_result,test_result)
     end
+
+    # *************** TASK 2 ***************
+
     def test_bay_for_item() # task 2
         test_result = bay_for_item("nail filer")
         actual_result = "b5"
         assert_equal(actual_result,test_result)
     end
+
+    # *************** TASK 3 ***************
+
     def test_items_in_multibay() # task 3
         test_result = items_in_multibay("b5, b10, b6")
         actual_result = ["nail filer", "cookie jar", "tooth paste"]
         assert_equal(actual_result,test_result)
     end
+
+    # *************** TASK 4 ***************
+
     def test_bays_for_multiitems() # task 4
         test_result = bays_for_multiitems("shoe lace, sharpie, hanger, rusty nail, leg warmers")
         actual_result = ["c9", "b2", "a4", "c1", "c10"]
         assert_equal(actual_result,test_result)
     end
+
+    # *************** TASK 5 ***************
+
     def test_calculate_distance_between_bays() # task 5
         test_result = calculate_distance_between_bays("b5, b10, b6")
         actual_result = [["nail filer", "cookie jar", "tooth paste"], 5]
         assert_equal(actual_result,test_result)
     end
+
     def test_calculate_distance_between_bays2() # task 5
         test_result = calculate_distance_between_bays("b3, c7, c9, a3")
         actual_result = [["picture frame", "paint brush", "shoe lace", "blouse"], 15]
         assert_equal(actual_result,test_result)
     end
+
+    # *************** TASK 6 ***************
+
     def test_order_bays_by_picking() # task 6
-        test_result = order_bays_by_picking("shoe lace, rusty nail, leg warmers")
+        test_result = order_bays_by_picking_from_items("shoe lace, rusty nail, leg warmers")
         actual_result = ["c1", "c9", "c10"]
         assert_equal(actual_result,test_result)
     end
+
     def test_order_bays_by_picking2() # task 6
-        test_result = order_bays_by_picking("hanger, deodorant, candy wrapper, rubber band")
+        test_result = order_bays_by_picking_from_items("hanger, deodorant, candy wrapper, rubber band")
         actual_result = ["a10", "a4", "c8", "b9"]
+        assert_equal(actual_result,test_result)
+    end
+
+    # *************** EXTRA ***************
+
+    def test_print_collection_map() # task 5
+        test_result = print_collection_map("b3, c7, c9, a3")
+        actual_result = [["picture frame", "paint brush", "shoe lace", "blouse"], 15]
         assert_equal(actual_result,test_result)
     end
 
